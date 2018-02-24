5fe56b49dda6eccd77af1f3a2c6731df83ed0f83
Rebecca Vernon <vernre01@luther.edu>
2018-02-24 16:17:28 -0600

    Made grid density flexible, chosen by the user.

--- script.js
+++ script.js
@@ -18,16 +18,36 @@ function boxColor() {
 	});
 }	
 */
-function clearGrid(e) {
+
+const filledSquare = document.querySelectorAll('.grid-item colored');
+
+//function clearGrid() {
 	const clearButton = document.querySelector('#cleargrid');
 	clearButton.addEventListener('click', (e) => {
-		squares.target.classList.remove('colored');
+		filledSquare.classList.remove('colored');
 	});
-}
+//}
 
-function setGridSize() {
+//function setGridSize(e) {
 	const sizeButton = document.querySelector('#gridpx');
 	sizeButton.addEventListener('click', (e) => {
+		let gridSize = prompt("Please enter a number between 1 and 100", "");
+		if (gridSize == null | gridSize == "") {
+		//	alert("okay.")
+		} else if (gridSize > 100) {
+			alert("Invalid entry. Please try again.");
+		} else {
+			for (i = 0; i < (gridSize*gridSize); i++) {
+				const squares = document.createElement('div');
+				squares.classList.add('grid-item');
+				container.style.cssText = 'grid-template-columns: repeat(' + gridSize + ', auto)';
+				squares.style.cssText = 'height: ' + 480/gridSize + 'px; width:' + 480/gridSize + 'px';
+				squares.addEventListener('mouseover', (event) => {
+					event.target.classList.add('colored');
+				});
 
+				container.appendChild(squares);
+			}
+		}
 	});
-}
+//}
