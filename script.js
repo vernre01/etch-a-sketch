177ca35406ab4a5f6b72b3366fe44256c349b6bc
Rebecca Vernon <vernre01@luther.edu>
2018-02-25 16:36:30 -0600

    Created functions to clarify code and fixed reset color button

--- script.js
+++ script.js
@@ -1,53 +1,47 @@
 const container = document.querySelector('.container');
+const gridSize = 16;
+const colorButton = document.querySelector('#rainbow');
+const sizeButton = document.querySelector('#gridpx');
+const clearButton = document.querySelector('#cleargrid');
 
-for (i = 0; i < 256; i++) {
-const squares = document.createElement('div');
-squares.classList.add('grid-item');
-squares.addEventListener('mouseover', (event) => {
-	event.target.classList.add('colored');
-	});
-
-container.appendChild(squares);
+function playGrid(gridSize) {
+	for (i = 0; i < (gridSize*gridSize); i++) {
+		const squares = document.createElement('div');
+		squares.classList.add('grid-item');
+		container.style.cssText = 'grid-template-columns: repeat(' + gridSize + ', auto)';
+		squares.style.cssText = 'height: ' + 480/gridSize + 'px; width:' + 480/gridSize + 'px';
+		squares.addEventListener('mouseover', (event) => {
+			event.target.classList.add('colored');
+		});
+		container.appendChild(squares);
+	}
 }
-/*
-function boxColor() {
-	const colorButton = document.querySelector('#rainbow');
-	if (
-	colorButton.addEventListener('click', (e) => {
 
-	});
-}	
+/*
+//function to change 'mouseover' color from black to random
+colorButton.addEventListener('click', (e) => {
+
+});
 */
 
-const filledSquare = document.querySelectorAll('.grid-item colored');
+//function to reset color of grid to white
+clearButton.addEventListener('click', (e) => {
+	let filledSquare = document.querySelectorAll('.grid-item.colored');
+	for (i = 0; i < filledSquare.length; i++) {
+		filledSquare[i].classList.remove('colored');
+	}
+});
 
-//function clearGrid() {
-	const clearButton = document.querySelector('#cleargrid');
-	clearButton.addEventListener('click', (e) => {
-		filledSquare.classList.remove('colored');
-	});
-//}
+//function that changes size of grid from default
+sizeButton.addEventListener('click', (e) => {
+	let gridSize = prompt("Please enter a number between 1 and 100", "");
+	if (gridSize == null | gridSize == "") {
+	//	don't do anything if blank or null
+	} else if (gridSize > 100) {
+		alert("Invalid entry. Please try again.");
+	} else {
+		playGrid(gridSize);
+	}		
+});
 
-//function setGridSize(e) {
-	const sizeButton = document.querySelector('#gridpx');
-	sizeButton.addEventListener('click', (e) => {
-		let gridSize = prompt("Please enter a number between 1 and 100", "");
-		if (gridSize == null | gridSize == "") {
-		//	alert("okay.")
-		} else if (gridSize > 100) {
-			alert("Invalid entry. Please try again.");
-		} else {
-			for (i = 0; i < (gridSize*gridSize); i++) {
-				const squares = document.createElement('div');
-				squares.classList.add('grid-item');
-				container.style.cssText = 'grid-template-columns: repeat(' + gridSize + ', auto)';
-				squares.style.cssText = 'height: ' + 480/gridSize + 'px; width:' + 480/gridSize + 'px';
-				squares.addEventListener('mouseover', (event) => {
-					event.target.classList.add('colored');
-				});
-
-				container.appendChild(squares);
-			}
-		}
-	});
-//}
+playGrid(gridSize);
